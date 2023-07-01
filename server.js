0c53f5c35f82794eecfb25196ecd287ad9d0c55c
mamianeji <mamia.neji@gmail.com>
2023-07-01 15:55:20 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require("express");
+
+const app=express() ; 
+
+app.use(express.json());
+
+const db_connect=require("./connect_db");
+
+require("dotenv").config();
+
+db_connect();
+
+app.use("/user", require("./routes/user"));
+app.listen(process.env.PORT,(err)=>err? console.log(err):console.log("server is running"));
